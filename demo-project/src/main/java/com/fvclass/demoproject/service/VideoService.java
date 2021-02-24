0959850f1594052e831c94e7fcdb70fc0661f613
FZVV <1027622693@qq.com>
2021-02-24 11:00:55 +0800

    get请求获取视频信息

---
+++ demo-project/src/main/java/com/fvclass/demoproject/service/VideoService.java
@@ -0,0 +1,14 @@
+package com.fvclass.demoproject.service;
+
+import com.fvclass.demoproject.domain.Video;
+
+import java.util.List;
+
+/**
+ * @author fv
+ */
+public interface VideoService {
+
+    List<Video> listVideo();
+
+}
